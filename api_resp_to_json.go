2097946c07d58b94e48e6d783f5658c9d4ca1d83
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-04-02 10:27:19 +0500

    language update

--- api_resp_to_json.go
+++ api_resp_to_json.go
@@ -17,59 +17,57 @@ func main() {
 	// URL API
 	// url := "https://api.sampleapis.com/beers/ale"
 
-	// Отправка GET запроса
+	// get
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка при выполнении GET запроса: %v\n", err)
+		fmt.Fprintf(os.Stderr, "get error: %v\n", err)
 		os.Exit(1)
 	}
 	defer resp.Body.Close()
 
-	// Проверка статуса ответа
+	// check ctatus
 	if resp.StatusCode != http.StatusOK {
-		fmt.Fprintf(os.Stderr, "Ошибка: %s\n", resp.Status)
+		fmt.Fprintf(os.Stderr, "error: %s\n", resp.Status)
 		os.Exit(1)
 	}
 
-	// Декодирование JSON ответа
+	// decode JSON
 	var result interface{}
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка при декодировании JSON: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error decode JSON: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Форматирование JSON с отступами
+	// format JSON
 	prettyJSON, err := json.MarshalIndent(result, "", "    ")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка при форматировании JSON: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error format json JSON: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Вывод JSON данных в терминал
+	// json to terminal
 	fmt.Println(string(prettyJSON))
 
-	// Получаем текущую дату и время
+	// current date
 	currentTime := time.Now()
-
-	// Форматируем текущую дату и время в строку
 	dateTimeStr := currentTime.Format("2006-01-02_15-04-05")
 
-	// Формируем имя файла с текущей датой и временем
+	// filename
 	fileName := fmt.Sprintf("json_out_%s.json", dateTimeStr)
 
-	// Создаем файл с указанным именем
+	// save to file
 	file, err := os.Create(fileName)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка при создании файла: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error crete file: %v\n", err)
 		os.Exit(1)
 	}
 	defer file.Close()
 
 	_, err = file.Write(prettyJSON)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Ошибка при записи в файл: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error record to file: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("Данные успешно записаны в файл output.json")
+	fmt.Println("done output.json")
 }
